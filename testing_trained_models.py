9502a0759713b4f18e7aabd660895fb0dbd351b2
Ole Jørgen Rise <ojrise@stud.ntnu.no>
2022-01-25 17:29:40 +0100

    delt opp trening og testing i to forskjellige koder

--- testing_trained_models.py
+++ testing_trained_models.py
@@ -12,20 +12,15 @@ from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
 
 
-def wrap_env(env):
-    wrapped_env = Monitor(env)                          # Needed for extracting eprewmean and eplenmean
-    wrapped_env = DummyVecEnv([lambda : wrapped_env])   # Needed for all environments (e.g. used for mulit-processing)
-    wrapped_env = VecNormalize(wrapped_env)             # Needed for improving training when using MuJoCo envs?
-    return wrapped_env
+filename = 'test'
 
-
-# Training
-env = GymWrapper(
+#Testing
+env_robo = GymWrapper(
         suite.make(
             env_name="Lift",
             robots = "IIWA",
             gripper_types="Robotiq85Gripper", 
-            has_renderer = False,
+            has_renderer = True,
             has_offscreen_renderer= False,
             use_camera_obs=False,
             use_object_obs=True,
@@ -36,16 +31,23 @@ env = GymWrapper(
         )
     )
 
-env = wrap_env(env)
-filename = 'test'
 
-model = PPO('MlpPolicy', env, verbose=1, tensorboard_log='./ppo_fetchpush_tensorboard/')
-print("starting to learn")
-model.learn(total_timesteps=200, tb_log_name=filename)
+# Load model
+model = PPO.load('trained_models/' + filename)
+# Load the saved statistics
+env = DummyVecEnv([lambda : env_robo])
+env = VecNormalize.load('trained_models/vec_normalize_' + filename + '.pkl', env)
+#  do not update them at test time
+env.training = False
+# reward normalization
+env.norm_reward = False
 
-print("finished learning")
+obs = env.reset()
 
-model.save('trained_models/' + filename)
-env.save('trained_models/vec_normalize_' + filename + '.pkl')     # Save VecNormalize statistics
+while True:
+    action, _states = model.predict(obs, deterministic=True)
+    obs, reward, done, info = env.step(action)
 
-
+    env_robo.render()
+    if done:
+        obs = env.reset()
