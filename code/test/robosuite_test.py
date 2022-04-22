74f72485b32a8e9af2003a8b4f5eef33ea26d5aa
ojrise <ojrise@stud.ntnu.no>
2022-04-22 09:44:14 +0200

    test for lage env med bilder

--- code/test/robosuite_test.py
+++ code/test/robosuite_test.py
@@ -2,16 +2,17 @@ import numpy as np
 import robosuite as suite
 
 # create environment instance
+print("make")
 env = suite.make(
     env_name="Lift",
     robots = "IIWA",
     gripper_types="Robotiq85Gripper",                # use default grippers per robot arm
-    has_renderer=True,                     # no on-screen rendering
-    has_offscreen_renderer=False,            # off-screen rendering needed for image obs
+    has_renderer=False,                     # no on-screen rendering
+    has_offscreen_renderer=True,            # off-screen rendering needed for image obs
     control_freq=20,                        # 20 hz control for applied actions
     horizon=200,                            # each episode terminates after 200 steps
     use_object_obs=False,                   # don't provide object observations to agent
-    use_camera_obs=False,                   # provide image observations to agent
+    use_camera_obs=True,                   # provide image observations to agent
     camera_names="agentview",               # use "agentview" camera for observations
     camera_heights=84,                      # image height
     camera_widths=84,                       # image width
@@ -19,10 +20,13 @@ env = suite.make(
 
 )
 
+print("finished")
 # reset the environment
 env.reset()
 
 for i in range(1000):
     action = np.random.randn(env.robots[0].dof) # sample random action
     obs, reward, done, info = env.step(action)  # take action in the environment
-    env.render() 
+    if done:
+        env.reset()
+    #env.render() 
